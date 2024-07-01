85f65f437a310f6f2458a258bd3e4c8d18bd07f8
JM-Feldman <jmfpriv@gmail.com>
2024-07-01 14:30:55 +0200

    AAT Developer Test submission

--- EventRegistrationApp/EventRegistrationApp/EventRegistrationApp/obj/Debug/net8.0/EventRegistrationApp.AssemblyInfo.cs
+++ EventRegistrationApp/EventRegistrationApp/EventRegistrationApp/obj/Debug/net8.0/EventRegistrationApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventRegistrationApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8ef0a2e596cf6616f7ea656ff4d51d6d556d21a")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventRegistrationApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventRegistrationApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- OptimizatingSQLQuery/OptimizatingSQLQuery/obj/Debug/net8.0/OptimizingSQLQuery.AssemblyInfo.cs
+++ OptimizatingSQLQuery/OptimizatingSQLQuery/obj/Debug/net8.0/OptimizingSQLQuery.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OptimizingSQLQuery")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8ef0a2e596cf6616f7ea656ff4d51d6d556d21a")]
 [assembly: System.Reflection.AssemblyProductAttribute("OptimizingSQLQuery")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OptimizingSQLQuery")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
